0d91004aa1db1b206b7b9088161c1cd28614d7ef
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-08-04 00:07:46 +0500

    Imlement association

--- student.rb
+++ student.rb
@@ -6,6 +6,7 @@ class Student < Person
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
+    classroom&.add_student(self)
   end
 
   def play_hooky
