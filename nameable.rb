762519c8238f39d6204fb3eb34f82872ff830897
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-08-03 22:21:22 +0500

    Implement decorators

---
+++ nameable.rb
@@ -0,0 +1,5 @@
+class Nameable
+  def correct_name
+    raise NotImplementedError, 'The correct_name method is not implemented.'
+  end
+end
--- person.rb
+++ person.rb
@@ -1,16 +1,24 @@
-class Person
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = rand(1..9999)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
   end
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
@@ -19,3 +27,14 @@ class Person
     @age >= 18
   end
 end
+
+
+# Testing the decorators
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
